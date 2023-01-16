942a7cf9139dfb00cbd91496f835eec9fb6d0657
papatenko <62730187+papatenko@users.noreply.github.com>
2023-01-16 13:20:18 -0600

    Update nomo9convert.ps1

--- scripts/other/nomo9convert.ps1
+++ scripts/other/nomo9convert.ps1
@@ -1,7 +1,7 @@
 $downloadsFolder = $HOME + '\Downloads'
 for (; ; ) {
-    Get-ChildItem $downloadsFolder'9convert - *' |
-    ForEach-Object {Rename-Item $_ -NewName ($_.name -replace '9convert - ', '')}
+    Get-ChildItem $downloadsFolder'9convert.com - *' |
+    ForEach-Object {Rename-Item $_ -NewName ($_.name -replace '9convert.com - ', '')}
 
     Start-Sleep 1 #in seconds
 } 
