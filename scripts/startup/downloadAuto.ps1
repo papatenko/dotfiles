1f3fd9cf4238865564b10b26b2c6b8b7edc3a62a
papatenko <62730187+papatenko@users.noreply.github.com>
2023-01-16 13:21:02 -0600

    Update downloadAuto.ps1

--- scripts/startup/downloadAuto.ps1
+++ scripts/startup/downloadAuto.ps1
@@ -42,8 +42,8 @@ foreach ($destination in $destinations) {
 for (; ; ) {
     Move-Item -Path $downloadsFolder$9Convert -Destination $ytVideosFolder
 
-    Get-ChildItem $ytVideosFolder'9convert - *' |
-    ForEach-Object {Rename-Item $_ -NewName ($_.name -replace '9convert - ', '')}
+    Get-ChildItem $ytVideosFolder'9convert.com - *' |
+    ForEach-Object {Rename-Item $_ -NewName ($_.name -replace '9convert.com - ', '')}
 
     foreach ($extension in $documentExtensions) {
         Move-Item -Path $downloadsFolder'\*.'$extension -Destination $documentsFolder
