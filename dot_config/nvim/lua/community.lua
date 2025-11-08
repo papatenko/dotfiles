8ed55348e1a2240a3d177bbecec2ae614365edbd
JK-le-dev <jkcondor13@gmail.com>
2025-11-08 09:27:37 -0600

    ugh

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -6,15 +6,9 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.editing-support.undotree" },
   { import = "astrocommunity.editing-support.chatgpt-nvim" },
-  { import = "astrocommunity.completion.codeium-vim" },
   -- import/override with your plugins folder
 }
